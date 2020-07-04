f56b826ca7255b69bb72c3a797097cb977dbb5c5
Danben Erick <danbencb08@outlook.es>
2020-07-04 04:01:01 -0500

    He dado algunos retoques al diseño
    Se creo la vista home

--- src/components/Pokemon/Home.jsx
+++ src/components/Pokemon/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import './style/home.css'
 const Home = () => {
     return (
-        <div>
-            <h1>Pagina Principal</h1>
-        </div>
+        <section>
+            <h1>Buscador de Pokemones</h1>
+            <div class="section__body">
+                <p>Esta es una aplicacion con el que puedes buscar pokemons por ID y Nombre</p>
+                <img src="https://i.pinimg.com/originals/6c/76/8e/6c768e9fff3f80e61f59cacfd397284c.png" alt=""/>
+            </div>
+        </section>
     )
 }
 export default Home
